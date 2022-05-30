4d238b6117e14790babb4319f24c64ad8d8c3790
Jose Santisteban <u201922760@upc.edu.pe>
2022-05-30 16:07:47 -0500

    Models

---
+++ src/app/models/post.model.ts
@@ -0,0 +1,7 @@
+import { User } from "./user.model";
+export class Post{
+    id?: number;
+    user?: User;
+    description?: string;
+    foto_post?: string;
+}
---
+++ src/app/models/proyecto.model.ts
@@ -0,0 +1,9 @@
+import { Group } from "./group.model";
+export class Proyecto{
+    id?: number;
+    name?: string;
+    url?: string;
+    foto?: string;
+    grupo?: Group;
+    description?: string;
+}
