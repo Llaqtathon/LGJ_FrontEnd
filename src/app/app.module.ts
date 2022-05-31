756808c1af06208e82f02963a56c7d793a866bc1
Maria Isabel Lopez <mrlpz1515@gmail.com>
2022-05-31 11:04:16 -0500

    small fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,7 +35,6 @@ import {
   GroupAddComponent, GroupDetailsComponent, 
   GroupsListComponent, GroupsComponent, GroupDeleteConfirmationDialog
 } from './components/groups';
-import { RegisterUserComponent } from './components/users';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
 import { MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
