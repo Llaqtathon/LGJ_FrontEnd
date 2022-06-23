ee38217eb9bb41770807717dceb6a39c95ed056f
Rodrigo Lopez <64718167+Rodrigoalt15@users.noreply.github.com>
2022-06-22 21:43:53 -0500

    Actualizacion registro

--- src/app/services/identity.service.ts
+++ src/app/services/identity.service.ts
@@ -16,12 +16,6 @@ export class IdentityService {
   }
 
   create(data: any): Observable<User> {
-    const send = {
-      ...data,
-        distrito: null,
-        distritoId: null,
-        rol: 0,
-    }
-    return this.http.post<User>(`${environment.baseUrl}/users`, send);
+    return this.http.post<User>(`${environment.baseUrl}/signup`, data);
 }
 }
